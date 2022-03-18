a2d366dd3f203dd36c46187ca65010de9c8ba604
muraliseshan5 <murali.seshan5@gmail.com>
2022-03-18 22:33:29 +0530

    [MS] Code 1

--- scripts/test.py
+++ scripts/test.py
@@ -1,1 +1,15 @@
-print("hello")
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('../data/sample.xml')
+root = tree.getroot()
+for child in root:
+    # print(child.tag, child.attrib)
+    for grandchild in child:
+        # print(grandchild.tag, grandchild.attrib)
+        if grandchild.attrib.get('name') == 'Colombia':
+            print(child.attrib.get('name'))
+            # for country in root.iter('country'):
+                # print(country.attrib.get('name'))
+# for neighbor in root.iter('neighbor'):
+    # print(neighbor.attrib.get('name'))
+print("welcome! Gundamma")
